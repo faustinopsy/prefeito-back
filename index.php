a76ca831e215b48509df1d826f9d46978a57a83f
faustinopsy <rodrigohipnose@gmail.com>
2024-08-13 13:13:24 -0300

    modified:   .htaccess 	modified:   config/my_database.db 	modified:   index.php

--- index.php
+++ index.php
@@ -8,10 +8,17 @@ use src\controllers\votoController;
 
 define('API_KEY', 'godNotExist');
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+//header("Access-Control-Allow-Origin: https://prefeitosp.faustinopsy.com");
+header("Access-Control-Allow-Origin: localhost:5500");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, X-API-KEY, HTTP_X_AUTHORIZATION");
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200); 
+    exit();
+}
+
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/');
 $dados_env = $dotenv->load();
@@ -24,6 +31,12 @@ $method = $_SERVER['REQUEST_METHOD'];
 switch ($uri) {
     case '/index.php/voto':
         if ($method == 'POST') {
+            $apiKey = $_SERVER['HTTP_X_API_KEY'] ?? '';
+            if ($apiKey !== API_KEY) {
+                http_response_code(403); 
+                echo json_encode(["message" => "Acesso negado: Chave API inválida"]);
+                exit();
+            }
             $data = json_decode(file_get_contents("php://input"), true);
             $controller = new VotoController($database);
             $response = $controller->storeVoto($data);
